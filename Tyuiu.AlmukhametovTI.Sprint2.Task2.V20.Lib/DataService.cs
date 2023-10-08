05188bd95e0e60d4cef75a819fade08343817d7e
TimaTi1771 <talmuhametov@yandex.ru>
2023-10-09 00:46:39 +0300

    Task2

---
+++ Tyuiu.AlmukhametovTI.Sprint2.Task2.V20.Lib/DataService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.AlmukhametovTI.Sprint2.Task2.V20.Lib
+{
+    public class DataService : ISprint2Task2V20
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if ((x >= 3) && (x <= 5) && (y >= 3) && (y <= 5))
+            {
+                res = true;
+            }
+
+            else if ((x == 3) && (y == 6))
+            {
+                res = true;
+            }
+
+            else if ((x >= 6) && (x <= 9) && (y >= 5) && (y <= 8))
+            {
+                res = true;
+            }
+
+            else if ((x == 9) && (y == 3) && (y == 4))
+            {
+                res = true;
+            }
+
+            else if ((x == 6) && (y >= 9) && (y <= 11))
+            {
+                res = true;
+            }
+
+            else if ((y == 11) && (x >= 3) && (x <= 6))
+            {
+                res = true;
+            }
+
+            else if ((x >= 9) && (x <= 12) && (y >= 8) && (y <= 12))
+            {
+                res = true;
+            }
+
+            else if ((y == 13) && (x >= 10) && (x <= 12))
+            {
+                res = true;
+            }
+
+            else if ((x == 13) && (y >= 6) && (y <= 8))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+            
+
+            
+        }
+    }
+}
---
+++ Tyuiu.AlmukhametovTI.Sprint2.Task2.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.AlmukhametovTI.Sprint2.Task2.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.AlmukhametovTI.Sprint2.Task2.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3513970e-013b-4914-bbf2-1db55a60e919")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
