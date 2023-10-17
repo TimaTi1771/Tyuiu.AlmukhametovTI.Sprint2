3d7aafdf844d1b0e67b4ee9ed3c66b5de0884a11
TimaTi1771 <talmuhametov@yandex.ru>
2023-10-17 22:06:24 +0300

    Task7

--- Tyuiu.AlmukhametovTI.Sprint2.Task7.V2.Lib/DataService.cs
+++ Tyuiu.AlmukhametovTI.Sprint2.Task7.V2.Lib/DataService.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.AlmukhametovTI.Sprint2.Task7.V2.Lib
         public bool CheckDotInShadedArea(double x, double y)
         {
             bool res;
-            if ((Math.Pow(x, 2) + Math.Pow(y, 2) >= -1) && (Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && ((y < 0) && (y >= -1) && (x > 0) && (x <= 1)))
+            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) && (y <= (x / 2)))
             {
                 res = true;
             }
